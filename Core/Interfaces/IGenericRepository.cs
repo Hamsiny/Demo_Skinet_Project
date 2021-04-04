5d522c65261294ec02e27bbe6780911744832d33
曲洪澎 <hamsiny@quhongpengdembp.lan>
2021-04-04 12:13:23 +1200

    Section 4 API Generic Repository

---
+++ Core/Interfaces/IGenericRepository.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Core.Entities;
+using Core.Specifications;
+
+namespace Core.Interfaces
+{
+    //create generic interface of repository
+    public interface IGenericRepository<T> where T : BaseEntity
+    {
+        //create two methods, one get by id, another one get all item as a list
+        Task<T> GetByIdAsync(int id);
+        Task<IReadOnlyList<T>> ListAllAsync();
+        Task<T> GetEntityWithSpec(ISpecification<T> spec);
+        Task<IReadOnlyList<T>> ListAsync(ISpecification<T> spec);
+    }
+}
