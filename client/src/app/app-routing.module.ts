174c1f3c6b4f3dd8201cbf3bf566b215555bf534
曲洪澎 <hamsiny@quhongpengdeMacBook-Pro.local>
2021-05-10 14:02:26 +1200

    Section 14 Client - Basket

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -11,7 +11,13 @@ const routes: Routes = [
   {path: 'server-error', component: ServerErrorComponent, data: {breadcrumb: 'Server Error'}},
   {path: 'not-found', component: NotFoundComponent, data: {breadcrumb: 'Not Found'}},
   //lazy loading for shop module, the shop module only loaded when we enter the shop path
-  {path: 'shop', loadChildren: () => import('./shop/shop.module').then(mod => mod.ShopModule), data: {breadcrumb: 'Shop'}},
+  {path: 'shop', loadChildren: () => import('./shop/shop.module')
+    .then(mod => mod.ShopModule), data: {breadcrumb: 'Shop'}},
+  //same for basket module
+  {path: 'basket', loadChildren: () => import('./basket/basket.module')
+    .then(mod => mod.BasketModule), data: {breadcrumb: 'Basket'}},
+  {path: 'checkout', loadChildren: () => import('./checkout/checkout.module')
+    .then(mod => mod.CheckoutModule), data: {breadcrumb: 'Checkout'}},
   {path: '**', redirectTo: 'not-found', pathMatch: 'full'}
 
 ];
--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
+import { BasketService } from './basket/basket.service';
 
 @Component({
   selector: 'app-root',
@@ -10,9 +11,18 @@ import { Component } from '@angular/core';
 export class AppComponent implements OnInit {
   title = 'Skinet';
 
-  constructor() {}
-
+  constructor(private basketService: BasketService) {}
+  
   ngOnInit(): void {
+    //when this component start, keep the basket item we add before, so when we refresh the page, we still get the basket items
+    const basketId = localStorage.getItem('basket_id');
+    if (basketId) {
+      this.basketService.getBasket(basketId).subscribe(() => {
+        console.log('initialised basket');
+      }, error => {
+        console.log(error);
+      });
+    }
   }
   
 }
