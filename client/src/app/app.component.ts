7cc6909262c008e1b3deee42e521f8efa089a843
曲洪澎 <hamsiny@quhongpengdeMacBook-Pro.local>
2021-05-18 21:21:03 +1200

    Section 17 Client Idendity

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
+import { AccountService } from './account/account.service';
 import { BasketService } from './basket/basket.service';
 
 @Component({
@@ -11,9 +12,24 @@ import { BasketService } from './basket/basket.service';
 export class AppComponent implements OnInit {
   title = 'Skinet';
 
-  constructor(private basketService: BasketService) {}
+  constructor(private basketService: BasketService, private accountService: AccountService) {}
   
   ngOnInit(): void {
+    this.loadBasket();
+    this.loadCurrentUser();
+  }
+
+  loadCurrentUser() {
+  //same as basket keep user login 
+    const token = localStorage.getItem('token');
+    this.accountService.loadCurrentUser(token).subscribe(() => {
+      console.log('loaded user'); 
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  loadBasket() {
     //when this component start, keep the basket item we add before, so when we refresh the page, we still get the basket items
     const basketId = localStorage.getItem('basket_id');
     if (basketId) {
